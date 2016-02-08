0f4687eb1486e51c9e77d129186e8ff666aed806
Student <student@RU-005.local>
2016-02-08 11:02:42 -0700

    Updated Forms

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 angular.module('calendarMod', [])
 angular.module('calendarMod')
 	.controller('calendarController', ['$scope', function($scope){
-		
+		$scope.newCustomer = {
+			name: $scope.customerName,
+			phone: $scope.phone,
+			email: $scope.email,
+			dogName: $scope.dogName,
+			breed: $scope.breed,
+			weight: $scope.weight,
+			service: $scope.service
+		}
+		$scope.service = "Select a Service"
+		$scope.subInfo = function() {
+			$scope.showCustomer = true
+		}
+		$scope.serviceBath = function() {
+			$scope.service = "Bath and Brush"
+		}
+		$scope.serviceFFT = function() {
+			$scope.service = "Face, Feet and Tail"
+		}
+		$scope.serviceFullGroom = function() {
+			$scope.service = "Full Groom"
+		}
 	}])
