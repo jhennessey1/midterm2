e099b806c95e6a5be31d9fc71c8b54e7b31a2e2b
Student <student@RU-005.local>
2016-02-08 13:55:02 -0700

    Added Dates

--- main.js
+++ main.js
@@ -8,11 +8,30 @@ angular.module('calendarMod')
 			dogName: $scope.dogName,
 			breed: $scope.breed,
 			weight: $scope.weight,
-			service: $scope.service
+			service: $scope.service,
+			groomers: $scope.groomers,
+			dates: []
 		}
+		$scope.greenMessage = "First, choose your groomer(s)"
+		$scope.groomers = []
 		$scope.service = "Select a Service"
+		$scope.selectedGroomers = function() {
+			if($scope.selectedGroomerAlli) {
+				$scope.groomers.push("Alli")
+			}
+			if($scope.selectedGroomerEmily) {
+				$scope.groomers.push("Emily")
+			}
+			if($scope.selectedGroomerAmber) {
+				$scope.groomers.push("Amber")
+			}
+			$scope.showForms = true
+			$scope.greenMessage = "Next, complete these forms."
+		}
 		$scope.subInfo = function() {
 			$scope.showCustomer = true
+			$scope.greenMessage = "Now choose the date."
+			$scope.showDates = true
 		}
 		$scope.serviceBath = function() {
 			$scope.service = "Bath and Brush"
@@ -23,4 +42,30 @@ angular.module('calendarMod')
 		$scope.serviceFullGroom = function() {
 			$scope.service = "Full Groom"
 		}
+		$scope.dates = []
+		var x = 7
+		var y = 0
+		var today = new Date()
+		$scope.addDates = function() {
+			for(var i = y; i < x; i++){
+				$scope.newDate = {
+					date: new Date().setDate(today.getDate() + i),
+					bookAppointment: false
+				}
+				$scope.dates.push($scope.newDate)
+				$scope.newDate = {}
+			}
+		}
+		$scope.bookTime = function(date) {
+			$scope.bookAppointment = true
+		}
+		$scope.moreDates = function() {
+			x += 7
+			y += 7
+			$scope.addDates()
+		}
+		$scope.pushAppointment = function(date){
+			$scope.newCustomer.date.push()
+		}
+		$scope.addDates()
 	}])
